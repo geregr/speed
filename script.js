f3773f5cec5f4945a78df1845c40bf1126ab8eb2
geregr <67203296+geregr@users.noreply.github.com>
2020-07-23 12:46:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+var Time=10
+var score=0
+var text=["hello","be","better","yuck","duck","kick","no","him","toe","me","world","the"]
+var f;
+
+function word(){
+    var nr =Math.random()*text.length;
+    nr = Math.floor(nr)
+    console.log(nr)
+    f=text[nr]
+    var e=document.getElementById('text').innerHTML = f;
+} 
+
+function time(){
+    Time--
+    document.getElementById("te").innerHTML=Time+"s"
+    if(Time===0){
+        // document.getElementById("text").innerHTML = ""
+        // document.getElementById("se").innerHTML = ""
+        // document.getElementById("te").innerHTML = ""
+        // document.getElementById("be").innerHTML = "you lose please press the button below to restart and keep playing"
+        // document.-("h").innerHTML = ""
+        document.getElementById("tu").innerHTML=`
+        <h1>you lose</h1>
+        your score is ${score}
+        <p>
+        </p>
+        <button id="ne" onclick="location.reload()">press me to restart the game</button>
+        `
+        Time++
+
+    }
+};
+
+word()
+setInterval(time,1000)
+
+document.getElementById("id").addEventListener("input",function(tea){
+    if (tea.target.value ===f){
+        word()
+        score++
+        document.getElementById("se").innerHTML = score
+
+        Time=10
+        time()
+        tea.target.value=""
+
+
+    }
+})
+
+//document.getElementById("ne").addEventListener("click",function(){
+//    score=0
+//})
